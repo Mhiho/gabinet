7ef1c293ea97e2281e7de6013a518b0c014148bc
Mhiho <m.pelka7@gmail.com>
2018-06-28 11:27:14 +0200

    thisIsWhatIwantedNowNavigation

--- src/App.js
+++ src/App.js
@@ -1,106 +1,26 @@
 import React, { Component, PropTypes } from 'react';
 import './App.css';
 import {List,Map} from 'immutable';
+import road from './assets/images/road.jpg';
+import Picture from './components/picture/picture';
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
+}
 
-    this.state = {
-      lines: new List(),
-      isDrawing: false
-    };
 
-    this.handleMouseDown = this.handleMouseDown.bind(this);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.handleMouseUp = this.handleMouseUp.bind(this);
-  }
-
-  componentDidMount() {
-    document.addEventListener("mouseup", this.handleMouseUp);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("mouseup", this.handleMouseUp);
-  }
-
-  handleMouseDown(mouseEvent) {
-    if (mouseEvent.button != 0) {
-      return;
-    }
-
-    const point = this.relativeCoordinatesForEvent(mouseEvent);
-
-    this.setState(prevState => ({
-      lines: prevState.lines.push(new List([point])),
-      isDrawing: true
-    }));
-  }
-
-  handleMouseMove(mouseEvent) {
-    if (!this.state.isDrawing) {
-      return;
-    }
-
-    const point = this.relativeCoordinatesForEvent(mouseEvent);
-
-    this.setState(prevState =>  ({
-      lines: prevState.lines.updateIn([prevState.lines.size - 1], line => line.push(point))
-    }));
-  }
-
-  handleMouseUp() {
-    this.setState({ isDrawing: false });
-  }
-
-  relativeCoordinatesForEvent(mouseEvent) {
-    const boundingRect = this.refs.drawArea.getBoundingClientRect();
-    return new Map({
-      x: mouseEvent.clientX - boundingRect.left,
-      y: mouseEvent.clientY - boundingRect.top,
-    });
-  }
-
-  render() {
+  render(){
     return (
-      <div
-        className="drawArea"
-        ref="drawArea"
-        onMouseDown={this.handleMouseDown}
-        onMouseMove={this.handleMouseMove}
-      >
-        <Drawing lines={this.state.lines} />
-      </div>
-    );
+        <div>
+          <Picture />
+        </div>
+    )
   }
+
 }
 
-function Drawing({ lines }) {
-  return (
-    <svg className="drawing">
-      {lines.map((line, index) => (
-        <DrawingLine key={index} line={line} />
-      ))}
-    </svg>
-  );
-}
 
-function DrawingLine({ line }) {
-  const color = '#'+Math.floor(Math.random()*16777215).toString(16);
-  const distance = Math.sqrt(Math.pow(5,2)+Math.pow(5,2));
-  const size = (Math.random() * 15) / distance;
-  const strokeWid = (Math.random()+20/10-0.5)*size+(1-Math.random()+30/20-0.5)*size;
-  console.log(distance);
-
-  const pathData = "M " +
-    line
-      .map(p => {
-        return `${p.get('x')} ${p.get('y')}`;
-      })
-      .join(" L ");
-
-  return <path className="path" style={{stroke: color, strokeWidth: strokeWid}} d={pathData} />;
-}
 
 
 export default App;
---
+++ src/containers/navigation/navigation.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { createStackNavigator } from 'react-navigation';
+
+class Navigation extends Component {
+
+render(){
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+
+}
